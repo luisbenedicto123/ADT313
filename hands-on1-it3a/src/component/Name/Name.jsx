b5602930166991ebc1985b29b39a9e431b891eff
Luis Benedicto <luismigs837@gmail.com>
2024-09-04 12:48:30 +0800

    my reactjs app

---
+++ hands-on1-it3a/src/component/Name/Name.jsx
@@ -0,0 +1,13 @@
+import './Name.css';
+
+function Name({ firstName, lastName }) {
+    return (
+        <div>
+            <h1 className='text-purple'>
+                {firstName} <span>{lastName}</span>
+            </h1>
+        </div>
+    )
+}
+
+export default Name;
